e60ab337b68d5b595445e7846e39326d99e0f1c0
toast254 <toast254@users.noreply.github.com>
2017-02-09 10:22:54 +0100

    Update AppClient.java

--- code/AppClient.java
+++ code/AppClient.java
@@ -3,15 +3,12 @@ import java.rmi.registry.*;
 
 public class AppClient {
     public static void main(String[] argv) {
-        int port = 10000;  // le port
-
+        int port = 10000;  // le port de connexion
         try {
             // on se connecte au serveur
             Registry registry = LocateRegistry.getRegistry(port);
-
             // on cree le stub pour connaitre les methodes utilisable sur le serveur "Fibonacci"
             Interface stub = (Interface) registry.lookup("Fibonacci");
-
             // on execute et on affiche le resultat
             System.out.println(stub.fibonacci(100));
         } catch (Exception e) {
