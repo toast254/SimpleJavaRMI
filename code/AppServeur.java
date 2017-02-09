277717e44ef954eaa3ec4cde795f20ccdb99ace5
toast254 <toast254@users.noreply.github.com>
2017-02-09 10:24:06 +0100

    Update AppServeur.java

--- code/AppServeur.java
+++ code/AppServeur.java
@@ -1,28 +1,18 @@
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
+import java.rmi.registry.*;
+import java.rmi.server.*;
 
-public class AppServeur
-{
-    public static void main(String[] argv)
-    {
-        // le port
-        int port = 10000;
-
-        try
-        {
+public class AppServeur {
+    public static void main(String[] argv) {
+        int port = 10000;  // le port d'ecoute
+        try {
             // creation du skeleton vers le traitement
             Interface skeleton = (Interface) UnicastRemoteObject.exportObject(new Serveur(true), port);
-
             // on cree le serveur
             Registry registry = LocateRegistry.createRegistry(port);
-
             // publication de notre serveur sous le nom "Fibonacci" avec le traitement defini dans notre skeleton
             registry.rebind("Fibonacci", skeleton);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
