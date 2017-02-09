c665d529ae9e046a5ac19ce0dbd6fb7cae2f5f3f
toast254 <toast254@users.noreply.github.com>
2017-02-09 10:30:09 +0100

    Update Serveur.java

--- code/Serveur.java
+++ code/Serveur.java
@@ -1,49 +1,38 @@
 
 import java.util.*;
 
-public class Serveur implements Interface
-{
+public class Serveur implements Interface {
     private HashMap <Integer,Double> fibo_results;
-    private int number;
-    private boolean fast_mode;
-    private boolean cache_mode;
+    private int number;  // dernier nombre calculé pour connaitre l'etat du cache
+    private boolean fast_mode;  // utilisation de l'algorithme rapide
+    private boolean cache_mode;  // utilisation d'un cache pour les resultats
 
-    // Constructor
-    public Serveur()
-    {
+    public Serveur() {  // Constructeur
         this(false, false);
     }
 
-    public Serveur(boolean fast_and_cache)
-    {
+    public Serveur(boolean fast_and_cache) {
         this(fast_and_cache, fast_and_cache);
     }
 
-    public Serveur(boolean fast, boolean cache)
-    {
+    public Serveur(boolean fast, boolean cache) {
         fast_mode = fast;
         cache_mode = cache && fast;
-
         fibo_results = new HashMap <Integer, Double> ();
-
         number = 1;
-
-        // known values
+        // known values :
         ajout_result(0,0);
         ajout_result(1,1);
     }
 
-    private void ajout_result(int n, double result)
-    {
+    private void ajout_result(int n, double result) {
         fibo_results.put(new Integer(n), new Double(result));
     }
 
-    // implements "Interface"
-    public double fibonacci(int n)
-    {
+
+    public double fibonacci(int n) {  // implements "Interface"
         if (n <= 0)
             return 0;
-
         if (fast_mode)
             if (cache_mode)
                 return loop_cached_fibonaci(n);
@@ -53,28 +42,19 @@ public class Serveur implements Interface
             return rec_fibonacci(n);
     }
 
-    // recursive based algorithm, complexity = 2^n
-    private double rec_fibonacci(int n)
-    {
+    private double rec_fibonacci(int n) {  // recursive based algorithm, complexity = 2^n
         if (n <= 1)
             return n;
         else
             return rec_fibonacci(n-1) + rec_fibonacci(n-2);
     }
 
-    // loop based algorithm, complexity =~ 2*n
-    private double loop_fibonacci(int n)
-    {
+    private double loop_fibonacci(int n) {  // loop based algorithm, complexity =~ 2*n
         if (n == 1 || n == 2)
-        {
             return 1;
-        }
-
         double fibo1=1, fibo2=1, result=1;
-
-        for (int i = 3; i <= n; i++)
-        {
-            result = fibo1 + fibo2; //Fibonacci number is sum of previous two Fibonacci number
+        for (int i = 3; i <= n; i++) {
+            result = fibo1 + fibo2;  // Fibonacci number is sum of previous two Fibonacci number
             fibo1 = fibo2;
             fibo2 = result;
 
@@ -82,28 +62,18 @@ public class Serveur implements Interface
         return result;
     }
 
-    // loop based algorithm with result caching, complexity =< "loop_fibonacci"
-    private double loop_cached_fibonaci(int n)
-    {
-        if (n <= number) // == fibo_results.contains(n)
+    private double loop_cached_fibonaci(int n) {  // loop based algorithm with result caching, complexity =< "loop_fibonacci"
+        if (n <= number)  // == fibo_results.contains(n)
             return fibo_results.get(n);
-
         double fibo1 = fibo_results.get(number-1);
         double fibo2 = fibo_results.get(number);
         double result = 0;
-
-        for (int i = number + 1; i <= n; i++)
-        {
-            // calculation details :
-            //~ System.out.println(i + " : " + fibo1 + " + " + fibo2 + " > " + result);
-
-            result = fibo1 + fibo2;
+        for (int i = number + 1; i <= n; i++) {
+            result = fibo1 + fibo2;  // Fibonacci number is sum of previous two Fibonacci number
             fibo1 = fibo2;
             fibo2 = result;
-
-            ajout_result(number = i, result);
+            ajout_result(number = i, result);  // garder en cache le resultat
         }
-
         return result;
     }
 }
